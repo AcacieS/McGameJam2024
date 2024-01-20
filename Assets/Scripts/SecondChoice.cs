a70e7ed8e7b5c515f41b17b16929f44e5c25c4e4
AcacieS <acaciesong@gmail.com>
2024-01-20 10:33:04 -0500

    script to enter another scene

--- Assets/Scripts/SecondChoice.cs
+++ Assets/Scripts/SecondChoice.cs
@@ -1,20 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SecondChoice : MonoBehaviour
 {
-    public GameObject playerWalk;
-    //public GameObject secondChoice;
-    // Start is called before the first frame update
+    //public GameObject playerWalk;
     void Start()
     {
-        // GameObject g = GameObject.Find("GameObject Name");
-        //Then you can grab the script:
-
-        //BombDrop bScript = g.GetComponent<BombDrop>();
-        //playerWalk = GameObject.Find("player").GetComponent<>()
-        //sphere_object = GameObject.Find("Sphere");
     }
 
     // Update is called once per frame
@@ -22,10 +15,8 @@ public class SecondChoice : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Y)){
             Debug.Log("2222Yes");
-            playerWalk.GetComponent<PlayerController>().enabled=true;
-            //hey.SetActive(true);
-            //secondChoice.SetActive(true);
-            
+            //playerWalk.GetComponent<PlayerController>().enabled=true;
+            SceneManager.LoadScene("test");
 
         }else if(Input.GetKeyDown(KeyCode.N)){
             Debug.Log("2222No");
