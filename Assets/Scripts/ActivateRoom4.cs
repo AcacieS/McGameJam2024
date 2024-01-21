6ed5edd4014f46c59a26dde53accdf1894b28dc3
AcacieS <acaciesong@gmail.com>
2024-01-21 11:26:23 -0500

    idk

--- Assets/Scripts/ActivateRoom4.cs
+++ Assets/Scripts/ActivateRoom4.cs
@@ -7,7 +7,7 @@ public class ActivateRoom4 : MonoBehaviour
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.name == "Goal1"&&collision.gameObject.name == "Goal2"&&collision.gameObject.name == "Goal3"&&collision.gameObject.name == "Goal4"&&collision.gameObject.name == "Goal5"){
+        if(collision.gameObject.name == "Goal1"&&collision.gameObject.name == "Goal2"){//&&collision.gameObject.name == "Goal3"&&collision.gameObject.name == "Goal4"&&collision.gameObject.name == "Goal5"){
             gameObject.GetComponent<Animator>().enabled = true;
         }
 
