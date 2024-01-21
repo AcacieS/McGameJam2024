0888842dd83dc5aedbac226ff90a2cc929774dd8
AcacieS <acaciesong@gmail.com>
2024-01-21 08:04:15 -0500

    idk

--- Assets/Scripts/transitionTutotoReal.cs
+++ Assets/Scripts/transitionTutotoReal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class transitionTutotoReal : MonoBehaviour
+public class TransitionTutotoReal : MonoBehaviour
 {
     public void PlayReal()
     {
