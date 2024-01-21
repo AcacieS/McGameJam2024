f024bd6812ddfd09c7359b5733beac8ed4a35f8c
AcacieS <acaciesong@gmail.com>
2024-01-20 20:08:54 -0500

    idk

--- Assets/FinishFirstRoom.cs
+++ Assets/FinishFirstRoom.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishFirstRoom : StateMachineBehaviour
 {
@@ -8,6 +9,7 @@ public class FinishFirstRoom : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log("hey");
+        SceneManager.LoadScene("room1Inside");
+        //Debug.Log("hey");
     }
 }
