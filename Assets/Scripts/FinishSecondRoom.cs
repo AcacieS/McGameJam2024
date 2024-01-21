48c3a32cb6305c7f2889a8a486e89ae06d36b400
AcacieS <acaciesong@gmail.com>
2024-01-20 23:04:37 -0500

    idk btw tuto and real

--- Assets/Scripts/FinishSecondRoom.cs
+++ Assets/Scripts/FinishSecondRoom.cs
@@ -9,6 +9,6 @@ public class FinishSecondRoom : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        SceneManager.LoadScene("room1Inside");
+        SceneManager.LoadScene("transitionBtwTutoReal");
     }
 }
