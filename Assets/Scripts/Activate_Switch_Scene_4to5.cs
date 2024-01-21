94328c3b5120801510657cdf88f5b0584ca5401e
AcacieS <acaciesong@gmail.com>
2024-01-21 06:54:25 -0500

    4to5

---
+++ Assets/Scripts/Activate_Switch_Scene_4to5.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate_Switch_Scene_4to5 : MonoBehaviour
+{
+    public GameObject s_door;
+    public GameObject b_door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        s_door.GetComponent<Animator>().enabled = true;
+        b_door.GetComponent<Animator>().enabled = true;
+    }
+}
